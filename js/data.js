af8f88da8226d616e229579e5b54d0665b6f029d
Jonathan Trang <jttrang96@gmail.com>
2022-02-24 23:21:20 -0800

    Added CSS to form and listeners.

--- js/data.js
+++ js/data.js
@@ -1,8 +1,20 @@
 /* exported data */
-
 var data = {
   view: 'entry-form',
   entries: [],
   editing: null,
   nextEntryId: 1
 };
+
+var previousData = localStorage.getItem('code-journal');
+
+if (previousData !== null) {
+  data = JSON.parse(previousData);
+}
+
+function windowBeforeUnload(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('code-journal', dataJSON);
+}
+
+window.addEventListener('beforeunload', windowBeforeUnload);
--- js/main.js
+++ js/main.js
@@ -1,2 +1,29 @@
 /* global data */
 /* exported data */
+
+var $photoUpdate = document.querySelector('.photo');
+var $title = document.querySelector('.title');
+var $photoUrl = document.querySelector('.photo-url');
+var $notes = document.querySelector('.notes');
+var $saveButton = document.querySelector('.save-button');
+
+// this will update the placeholder photo
+function photoUpdate(event) {
+  $photoUpdate.setAttribute('src', event.target.value);
+}
+
+// this will submit a new submission
+function newSubmission(event) {
+  var title = $title.getElementById('title', event.target.value);
+  var photoUrl = $photoUrl.getElementById('photo-url', event.target.value);
+  var notes = $notes.getElementById('notes', event.target.value);
+  var submission = {
+    title,
+    photoUrl,
+    notes
+  };
+  console.log(submission);
+}
+
+$photoUrl.addEventListener('input', photoUpdate);
+$saveButton.addEventListener('submit', newSubmission);
