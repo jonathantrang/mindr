99eed0448607796828a0724f850aeaa772302904
Jonathan Trang <jttrang96@gmail.com>
2022-03-04 13:17:37 -0800

    Revised according to change suggestions.

--- js/data.js
+++ js/data.js
@@ -1,35 +1,19 @@
 /* exported data */
-var $form = document.querySelector('#entry-form');
-var $title = document.querySelector('.title');
-var $photoUrl = document.querySelector('.photo-url');
-var $notes = document.querySelector('.notes');
 var data = {
   view: 'entry-form',
   entries: [],
   editing: null,
   nextEntryId: 1
 };
+
 var previousData = localStorage.getItem('code-journal-entries');
 
 if (previousData !== null) {
   data.entries = JSON.parse(previousData);
 }
 
-function checkSubmit(event) {
-  event.preventDefault();
-  var submission = {
-    Title: $title.value,
-    'Photo URL': $photoUrl.value,
-    Notes: $notes.value,
-    nextEntryId: data.nextEntryId
-  };
-  var submissionEntry = JSON.stringify(submission);
-  data.entries.push(submissionEntry);
+function windowBeforeUnload(event) {
   localStorage.setItem('code-journal-entries', JSON.stringify(data.entries));
-  data.nextEntryId++;
-  if (data.nextEntryId++) {
-    $form.reset();
-  }
 }
 
-$form.addEventListener('submit', checkSubmit);
+window.addEventListener('beforeunload', windowBeforeUnload);
--- js/main.js
+++ js/main.js
@@ -1,11 +1,29 @@
 /* global data */
 /* exported data */
 
-var $photoUpdate = document.querySelector('.photo');
+var $form = document.querySelector('#entry-form');
+var $title = document.querySelector('.title');
 var $photoUrl = document.querySelector('.photo-url');
+var $notes = document.querySelector('.notes');
+var $photoUpdate = document.querySelector('.photo');
 
 function photoUpdate(event) {
   $photoUpdate.setAttribute('src', event.target.value);
 }
 
+function checkSubmit(event) {
+  event.preventDefault();
+  var submission = {
+    title: $title.value,
+    photoUrl: $photoUrl.value,
+    notes: $notes.value,
+    entryId: data.nextEntryId
+  };
+  data.nextEntryId++;
+  data.entries.push(submission);
+  $photoUpdate.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
+}
+
+$form.addEventListener('submit', checkSubmit);
 $photoUrl.addEventListener('input', photoUpdate);
