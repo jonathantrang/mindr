d4992f747b6e5ca84d5efc5fb8e2c3e1db0d026b
Jonathan Trang <jttrang96@gmail.com>
2022-03-04 01:10:43 -0800

    Completed issue-1-creating-an-entry.

--- js/data.js
+++ js/data.js
@@ -1,19 +1,35 @@
 /* exported data */
+var $form = document.querySelector('#entry-form');
+var $title = document.querySelector('.title');
+var $photoUrl = document.querySelector('.photo-url');
+var $notes = document.querySelector('.notes');
 var data = {
   view: 'entry-form',
   entries: [],
   editing: null,
   nextEntryId: 1
 };
-var previousData = localStorage.getItem('code-journal');
+var previousData = localStorage.getItem('code-journal-entries');
 
 if (previousData !== null) {
-  data = JSON.parse(previousData);
+  data.entries = JSON.parse(previousData);
 }
 
-function windowBeforeUnload(event) {
-  var dataJSON = JSON.stringify(data);
-  localStorage.setItem('code-journal', dataJSON);
+function checkSubmit(event) {
+  event.preventDefault();
+  var submission = {
+    Title: $title.value,
+    'Photo URL': $photoUrl.value,
+    Notes: $notes.value,
+    nextEntryId: data.nextEntryId
+  };
+  var submissionEntry = JSON.stringify(submission);
+  data.entries.push(submissionEntry);
+  localStorage.setItem('code-journal-entries', JSON.stringify(data.entries));
+  data.nextEntryId++;
+  if (data.nextEntryId++) {
+    $form.reset();
+  }
 }
 
-window.addEventListener('beforeunload', windowBeforeUnload);
+$form.addEventListener('submit', checkSubmit);
--- js/main.js
+++ js/main.js
@@ -3,39 +3,9 @@
 
 var $photoUpdate = document.querySelector('.photo');
 var $photoUrl = document.querySelector('.photo-url');
-var $saveButton = document.querySelector('.save-button');
-var $title = document.querySelector('.title');
-var $notes = document.querySelector('.notes');
 
 function photoUpdate(event) {
   $photoUpdate.setAttribute('src', event.target.value);
 }
 
-function checkSubmit(event) {
-  if (event.target === $saveButton) {
-    var submission = {
-      $title,
-      $photoUrl,
-      $notes
-    };
-    localStorage.setItem(submission);
-  }
-}
-
-$photoUrl.addEventListener('input', photoUpdate, checkSubmit);
-$title.addEventListener('input', checkSubmit);
-$notes.addEventListener('input', checkSubmit);
-$saveButton.addEventListener('submit', checkSubmit);
-
-/*
-  1. Prompt
-    a. Listen for a submit event on the journal entry form
-  2. How to get there?
-    a. How to check for submit events?
-    b. What does this submit event do?
-  3. Write steps to achieve without code
-    a. Have variables that check when event occurs on the save button
-    a. Create a function for the event
-    b. Have an addEventListener method that checks for the submit event and the callback function when the event occurs
-
-*/
+$photoUrl.addEventListener('input', photoUpdate);
